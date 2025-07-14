ab3d9ad1d326d137267764a667344ddf0dc46c8c
tapeds <frrllmatthew@gmail.com>
2025-07-14 10:10:34 +0700

    feat: add texture to catcher

--- keruh/Sources/Models/Catcher.swift
+++ keruh/Sources/Models/Catcher.swift
@@ -8,50 +8,53 @@
 import SpriteKit
 
 class Catcher: BaseGameObject {
-    private let shapeNode: SKShapeNode
+    private let spriteNode: SKSpriteNode
 
-    static let size = CGSize(width: 80, height: 20)
+    static let size = CGSize(width: 80, height: 60)
 
     init() {
-        self.shapeNode = SKShapeNode(
-            rectOf: Catcher.size,
+        self.spriteNode = SKSpriteNode(
+            texture: nil,
+            color: .clear,
+            size: Catcher.size
         )
         super.init(size: Catcher.size)
-        node.addChild(shapeNode)
+        node.addChild(spriteNode)
     }
 
     override func setup() {
-        shapeNode.fillColor = GameColors.catcher
-        shapeNode.strokeColor = GameColors.catcherStroke
-        shapeNode.lineWidth = 2
+        // Load texture from assets
+        spriteNode.texture = SKTexture(imageNamed: "catcher")
+        spriteNode.size = Catcher.size
+        spriteNode.zPosition = 1
 
-        // Physics
-        shapeNode.physicsBody = SKPhysicsBody(rectangleOf: size)
-        shapeNode.physicsBody?.isDynamic = false
-        shapeNode.physicsBody?.categoryBitMask = PhysicsCategory.catcher
-        shapeNode.physicsBody?.contactTestBitMask =
+        // Setup physics
+        spriteNode.physicsBody = SKPhysicsBody(rectangleOf: Catcher.size)
+        spriteNode.physicsBody?.isDynamic = false
+        spriteNode.physicsBody?.categoryBitMask = PhysicsCategory.catcher
+        spriteNode.physicsBody?.contactTestBitMask =
             PhysicsCategory.fallingObject
-        shapeNode.physicsBody?.collisionBitMask = 0
+        spriteNode.physicsBody?.collisionBitMask = 0
     }
 
     func moveTo(x: CGFloat, constrainedTo bounds: CGSize) {
         let constrainedX = max(
-            (Catcher.size.width / 2),
-            min(bounds.width - (Catcher.size.width / 2), x)
+            Catcher.size.width / 2,
+            min(bounds.width - Catcher.size.width / 2, x)
         )
         node.position.x = constrainedX
     }
-    
-//    func animateCatch() {
-//        let scaleUp = SKAction.scale(
-//            to: GameConfiguration.catcherScaleAnimation,
-//            duration: GameConfiguration.animationDuration
-//        )
-//        let scaleDown = SKAction.scale(
-//            to: 1.0,
-//            duration: GameConfiguration.animationDuration
-//        )
-//        let sequence = SKAction.sequence([scaleUp, scaleDown])
-//        node.run(sequence)
-//    }
+
+    //    func animateCatch() {
+    //        let scaleUp = SKAction.scale(
+    //            to: GameConfiguration.catcherScaleAnimation,
+    //            duration: GameConfiguration.animationDuration
+    //        )
+    //        let scaleDown = SKAction.scale(
+    //            to: 1.0,
+    //            duration: GameConfiguration.animationDuration
+    //        )
+    //        let sequence = SKAction.sequence([scaleUp, scaleDown])
+    //        node.run(sequence)
+    //    }
 }
